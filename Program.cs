fc75744b10ad2735c2c6350ffe6fcf4a6eb33ec7
Brethland <brethland@gmail.com>
2022-03-16 09:10:22 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stunde1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace Stunde_1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //Console.Writeline("Hallo Welt");
+                //Datentypen: ganze Zahlen; Dezimahlzahlen, Zeichenketten, Zeichen, Wahrheitswerte (Boolean)
+
+                // ganze Zahlen:
+                int ganzeZahlA = 41;
+                int ganzeZahlB;
+                ganzeZahlB = 7;
+                short kleineGanzeZahl;
+                long großGanzeZahl;
+
+
+
+                int ganzeZahlC = ganzeZahlA % ganzeZahlB;
+                Console.WriteLine(ganzeZahlC);
+
+                //Gleitkommazahlen (Dezimahlzahlen)
+                double gleitkommaZahlA = 1;
+                double gleitkommaZahlB = 3;
+                decimal großegleitkommazahl;
+
+                Console.WriteLine(gleikomma / gleitkommaZahlB)
+    
+            double gleitkommazahlC = ganzeZahlC + gleitkommaZahlA;
+                // int zu double -> kein Problwm
+                // double zu int -> braucht Eingreifen
+                Console.WriteLine(ganzeZahlC + gleitkommaZahlA);
+                bool wahrheitswert = true;
+                // später mehr
+
+                //Zeichen und Zeichenketten
+                char Zeichen = `&`;
+                string zeichenketteA = "Hallo Welt"
+                string zeichenketteB = "Hallo Welt"
+    
+            // string + string := konkatenation
+                Console.WriteLine(zeichenketteA + zeichenketteB);
+
+            }
+        }
+    }
+
+}
+    }
+}
